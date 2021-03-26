4b39d97dbcac889dc990cc17ad75be45ea327fd6
prakashdk <dprakash482@gmail.com>
2021-03-26 09:35:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Img from './Images/wishing.gif';
+
+function App() {
+  const name="Manoj";
+  return (
+    <>
+    <div className="body">
+      <h1 className="content">Hi, {name}!</h1>
+      <img alt="Loading..." width="90%" className="img" src={Img}></img>
+      
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
