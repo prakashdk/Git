cdaaedb095f1d35e819bb24ba136b6b44e2b6098
prakashdk <dprakash482@gmail.com>
2021-03-26 08:50:19 +0530

    Updates

---
+++ precache-manifest.7335a4f075a2fcc9a1a22d5912eded90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d9176ea1cd4e2d4ebcb01fd2e9c3f3",
+    "url": "/Git/index.html"
+  },
+  {
+    "revision": "9bdf5b3c19a01d119562",
+    "url": "/Git/static/css/main.a657a209.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Git/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Git/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdf5b3c19a01d119562",
+    "url": "/Git/static/js/main.c058c1a4.chunk.js"
+  },
+  {
+    "revision": "d30ef27dcbf19d20ad7e",
+    "url": "/Git/static/js/runtime-main.0168353b.js"
+  },
+  {
+    "revision": "e4d2c1d0da356797359acd9270bcdd77",
+    "url": "/Git/static/media/raining.e4d2c1d0.gif"
+  },
+  {
+    "revision": "a07cd56722c2a42b564afb3b8ad73707",
+    "url": "/Git/static/media/wishing.a07cd567.gif"
+  }
+]);
